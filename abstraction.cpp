0624e1e26ccc67dddccaa9e31c944ba9946c6f97
Roshan Rino <roshanrino.me19@bitsathy.ac.in>
2022-04-18 16:56:34 +0530

    Data Hiding

--- abstraction.cpp
+++ abstraction.cpp
@@ -2,22 +2,22 @@
 using namespace std;
 class time
 {
-    short data=0;
+    short data;
 public:
     time(int a , int b)
     {
-        data=a;
-        data<<=6;
-        data+=b;
+        data=(a<<6)+b;
     }
     void printData()
     {
-        cout<<(data>>6)<<" : "<<data%64;
+        cout<<(data>>6)<<" : "<<data%64<<endl;
     }
 };
 int main()
 {
     time t(12,55);
+    time t2(10,12);
     t.printData();
+    t2.printData();
     return 0;
 }
